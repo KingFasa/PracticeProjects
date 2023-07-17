794d8f88019fc64b72f70ecccf5e0c3740b016d6
Jamie Walker <jwalker5@mail.sfsu.edu>
2023-07-17 10:13:00 -0400

    Started: Find Pi to Nth Digit project

---
+++ Projects/Numbers/FindPiNthDigit/FindPiNthDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindPiNthDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

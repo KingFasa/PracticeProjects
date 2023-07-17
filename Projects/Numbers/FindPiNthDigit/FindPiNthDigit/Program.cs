f25d75d4cca8e322217e3aeb6e3628d87fc51f33
Jamie Walker <jwalker5@mail.sfsu.edu>
2023-07-17 18:21:30 -0400

    Researched Methods to find Pi

--- Projects/Numbers/FindPiNthDigit/FindPiNthDigit/Program.cs
+++ Projects/Numbers/FindPiNthDigit/FindPiNthDigit/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Find PI to the Nth Digit:
+                Enter a number and have the program generate
+                PI up to that many decimal places.
+                Keep a limit to how far the program will go. */
+
+            /*
+             How to find PI?
+             */
+
+            Console.WriteLine("How many digits of PI would you like to find?");
+            Console.WriteLine("");
         }
+
+        static private float GregorySeriesMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        static private float ArchimedesMethod() { throw new NotImplementedException(); }
+
+        static private float Machin() { throw new NotImplementedException(); }
+
+        static private float ChudnovskyMethod() 
+        {
+            float piToNthDigit = 0;
+
+
+
+
+
+            return piToNthDigit;
+        }
+
+
+
     }
 }
